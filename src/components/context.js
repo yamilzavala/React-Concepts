fed3e16ccdb53ad5d699dde870de939c1032302b
yamilzavala <zavalayamil9@gmail.com>
2021-08-11 14:28:03 -0300

    useContext

--- src/components/context.js
+++ src/components/context.js
@@ -1,4 +1,4 @@
-import {createContext, useContext} from 'react';
+import {createContext, useContext, useState} from 'react';
 const Context = createContext();
 
 function Child(props) {
@@ -7,13 +7,13 @@ function Child(props) {
 
 function GrandChild(props) {
     const context = useContext(Context);
-    return <h1>{context}</h1>
+    return <h1>{JSON.stringify(context)}</h1>
 }
 
 export default function ContextComponent() {    
-   
+    const [data, setData] = useState({nombre: 'yamil', apellido: 'zavala'});
     return  (
-        <Context.Provider value={'Yamil'}>
+        <Context.Provider value={data}>
             <Child/>
         </Context.Provider>   
     )
